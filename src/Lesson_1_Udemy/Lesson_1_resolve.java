e41d024efba5c46638df34e5fca7b6b348d7217b
KolyaSamandasiuk <kolyasamandasyk@gmail.com>
2023-01-31 18:22:10 +0200

    add resolve file for lesson1(Udemy)

---
+++ src/Lesson_1_Udemy/Lesson_1_resolve.java
@@ -0,0 +1,17 @@
+package Lesson_1_Udemy;
+
+public class Lesson_1_resolve {
+
+    public static void main(String[] args) {
+
+        System.out.println("             Текст             ");
+        System.out.println("Текст текст текст текст текст текст текст.");
+        System.out.println("Текст текст текст текст.");
+        System.out.println("Текст текст текст текст текст текст!");
+        System.out.println("Текст текст.");
+        System.out.println("                           Author Name Surname");
+
+    }
+
+
+}
