6bc0d5b73f8fc9298c642a64234230530df795dd
KolyaSamandasiuk <kolyasamandasyk@gmail.com>
2023-01-31 18:49:09 +0200

    add some code to lesson2 resolve

---
+++ src/Lesson_2_Udemy/Lesson_2_resolve.java
@@ -0,0 +1,32 @@
+package Lesson_2_Udemy;
+
+public class Lesson_2_resolve {
+
+    public static void main(String[] args) {
+
+        byte a1, b1, c1, d1;
+
+        a1 = 0B1100;
+        b1 = 014;
+        c1 = 12;
+        d1 = 0xC;
+
+        System.out.println(a1);
+        System.out.println(b1);
+        System.out.println(c1);
+        System.out.println(d1);
+
+        short a2, b2, c2, d2;
+
+        a2 = 0b10100010100;
+        b2 = 02424;
+        c2 = 1300;
+        d2 = 0x514;
+
+        System.out.println(a2);
+        System.out.println(b2);
+        System.out.println(c2);
+        System.out.println(d2);
+    }
+
+}
