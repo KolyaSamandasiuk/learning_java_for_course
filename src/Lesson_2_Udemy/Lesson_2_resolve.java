5382d03ee8fac5534708fee192388f249af187ff
KolyaSamandasiuk <kolyasamandasyk@gmail.com>
2023-02-01 19:19:18 +0200

    add resolsed task for lesson2

--- src/Lesson_2_Udemy/Lesson_2_resolve.java
+++ src/Lesson_2_Udemy/Lesson_2_resolve.java
@@ -27,6 +27,65 @@ public class Lesson_2_resolve {
         System.out.println(b2);
         System.out.println(c2);
         System.out.println(d2);
+
+        int a3, b3, c3, d3;
+
+        a3 = 0b0;
+        b3 = 00;
+        c3 = 0;
+        d3 = 0x0;
+
+        System.out.println(a3);
+        System.out.println(b3);
+        System.out.println(c3);
+        System.out.println(d3);
+
+        long a4, b4, c4, d4;
+
+        a4 = 0b101111000110000101001110L;
+        b4 = 057060516L;
+        c4 = 12345678L;
+        d4 = 0xBC614EL;
+
+        System.out.println(a4);
+        System.out.println(b4);
+        System.out.println(c4);
+        System.out.println(d4);
+
+        float a5, b5;
+
+        a5 = 1.25f;
+        b5 = 2.28f;
+
+        System.out.println(a5);
+        System.out.println(b5);
+
+        double a6, b6;
+
+        a6 = 4.25D;
+        b6 = 8.28D;
+
+        System.out.println(a6);
+        System.out.println(b6);
+
+        boolean bool_true, bool_false;
+
+        bool_true = true;
+        bool_false = false;
+
+        System.out.println(bool_true);
+        System.out.println(bool_false);
+
+        char ch1, ch2, ch3;
+
+        ch1 = 'S';
+        ch2 = '\u0555';
+        ch3 = 221;
+
+        System.out.println(ch1);
+        System.out.println(ch2);
+        System.out.println(ch3);
+
     }
 
 }
